f688462ef72f3f70553d71aeb8f60ae06f738d9c
anirudh09041 <anirudh09041@gmail.com>
2021-04-07 14:20:37 +0530

    slash added

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
+var ejs = require('ejs');
 const app = express();
 var PORT = process.env.PORT || 4000;
 app.use(cors());
+app.engine('html', ejs.renderFile);
 
 app.listen(PORT, () => {
     console.log("server is running ");
 });
-app.get('/',(req,res)=>{
-    res.render('index.html');
+app.get('*',(req,res)=>{
+    res.render('/index.html');
     var URL = req.query.URL; 
     res.header('Content-Disposition','attachment; filename="video.mp4');
     ytdl(URL,{format: 'mp4'}).pipe(res);
